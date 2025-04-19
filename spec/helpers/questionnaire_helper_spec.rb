e578055d6dc591c168aac67852bb42e068c71f66
Ishani Rajput <irajput@vclvm177-179.vcl.ncsu.edu>
2025-04-19 15:11:57 -0400

    Test cases added for adjust_advice_size in QuestionnaireHelper

--- spec/helpers/questionnaire_helper_spec.rb
+++ spec/helpers/questionnaire_helper_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe QuestionnaireHelper, type: :helper do
+  require 'rails_helper'
+
   describe '.adjust_advice_size' do
     let(:questionnaire) { double('Questionnaire', min_question_score: 1, max_question_score: 3) }
-    let(:question) { double('ScoredQuestion', id: 1, question_advices: []) }
+    let(:question) { double('ScoredQuestion', id: 101, question_advices: []) }
 
     before do
       allow(question).to receive(:is_a?).with(ScoredQuestion).and_return(true)
     end
 
     context 'when question is not a ScoredQuestion' do
-      it 'does not make any changes' do
+      it 'does not perform any operations' do
         non_scored_question = double('Question')
         expect(non_scored_question).to receive(:is_a?).with(ScoredQuestion).and_return(false)
         expect(QuestionAdvice).not_to receive(:delete)
@@ -19,104 +21,87 @@ RSpec.describe QuestionnaireHelper, type: :helper do
     end
 
     context 'when question is a ScoredQuestion' do
-      it 'removes QuestionAdvice entries outside the valid score range' do
-        expect(QuestionAdvice).to receive(:delete).with(['question_id = ? AND (score > ? OR score < ?)', question.id, 3, 1])
+      it 'deletes advice entries outside valid score range' do
+        expect(QuestionAdvice).to receive(:delete).with(
+          ['question_id = ? AND (score > ? OR score < ?)', question.id, 3, 1]
+        )
         allow(QuestionAdvice).to receive(:where).and_return([])
 
         QuestionnaireHelper.adjust_advice_size(questionnaire, question)
       end
 
-      it 'creates missing advice and removes duplicates' do
+      it 'adds missing advice and deletes duplicates' do
+        # Simulate score range 1..3
         (1..3).each do |score|
-          fake_qas = double('ActiveRecord::Relation', first: nil, size: 0)
-          allow(QuestionAdvice).to receive(:where).with('question_id = ? AND score = ?', question.id, score).and_return(fake_qas)
+          qas = double('ActiveRecord::Relation', first: nil, size: 0)
+          allow(QuestionAdvice).to receive(:where).with('question_id = ? AND score = ?', question.id, score).and_return(qas)
           expect(question.question_advices).to receive(:<<).with(instance_of(QuestionAdvice))
         end
+
         allow(QuestionAdvice).to receive(:delete)
 
         QuestionnaireHelper.adjust_advice_size(questionnaire, question)
       end
     end
-  end
 
-  describe '#update_questionnaire_questions' do
-    controller(ApplicationController) do
-      include QuestionnaireHelper
+    context 'when valid advice exists for all scores in range' do
+      it 'does not create or delete any advice' do
+        (1..3).each do |score|
+          advice = double('QuestionAdvice')
+          qas = [advice]
+          allow(QuestionAdvice).to receive(:where).with('question_id = ? AND score = ?', question.id, score).and_return(qas)
+          expect(question.question_advices).not_to receive(:<<)
+          expect(QuestionAdvice).not_to receive(:delete).with(['question_id = ? AND score = ?', question.id, score])
+        end
+    
+        allow(QuestionAdvice).to receive(:delete).with(any_args) # for the outer delete, still expectable
+        QuestionnaireHelper.adjust_advice_size(questionnaire, question)
+      end
     end
-
-    before do
-      @question1 = double('Question', id: 1)
-      @question2 = double('Question', id: 2)
-
-      allow(Question).to receive(:find).with("1").and_return(@question1)
-      allow(Question).to receive(:find).with("2").and_return(@question2)
+    
+    context 'when multiple advice entries exist for a score' do
+      it 'deletes duplicate entries' do
+        (1..3).each do |score|
+          advice = double('QuestionAdvice')
+          qas = [advice, advice]
+          allow(QuestionAdvice).to receive(:where).with('question_id = ? AND score = ?', question.id, score).and_return(qas)
+          expect(QuestionAdvice).to receive(:delete).with(['question_id = ? AND score = ?', question.id, score])
+        end
+    
+        allow(question.question_advices).to receive(:<<)
+        allow(QuestionAdvice).to receive(:delete).with(['question_id = ? AND (score > ? OR score < ?)', question.id, 3, 1])
+        QuestionnaireHelper.adjust_advice_size(questionnaire, question)
+      end
     end
-
-    it 'returns early if params[:question] is nil' do
-      allow(controller).to receive(:params).and_return({})
-      expect(@question1).not_to receive(:save)
-      controller.update_questionnaire_questions
-    end
-
-    it 'updates changed attributes and saves the question' do
-      allow(@question1).to receive(:send).with('txt').and_return('Old text')
-      allow(@question1).to receive(:send).with('weight').and_return('1')
-      expect(@question1).to receive(:send).with('txt=', 'Updated question text')
-      expect(@question1).to receive(:send).with('weight=', '2')
-      expect(@question1).to receive(:save)
-
-      allow(@question2).to receive(:send).with('txt').and_return('Another text')
-      allow(@question2).to receive(:send).with('weight').and_return('1')
-      expect(@question2).not_to receive(:send).with('txt=', anything)
-      expect(@question2).not_to receive(:send).with('weight=', anything)
-      expect(@question2).to receive(:save)
-
-      allow(controller).to receive(:params).and_return({
-        question: {
-          "1" => { "txt" => "Updated question text", "weight" => "2" },
-          "2" => { "txt" => "Another text", "weight" => "1" }
-        }
-      })
-
-      controller.update_questionnaire_questions
-    end
-  end
-
-  describe '#questionnaire_factory' do
-    controller(ApplicationController) do
-      include QuestionnaireHelper
-    end
-
-    before do
-      stub_const("ReviewQuestionnaire", Class.new)
-      stub_const("SurveyQuestionnaire", Class.new)
-      controller.class.const_set("QUESTIONNAIRE_MAP", {
-        'ReviewQuestionnaire' => ReviewQuestionnaire,
-        'SurveyQuestionnaire' => SurveyQuestionnaire
-      })
-    end
-
-    it 'returns the correct questionnaire instance for a valid type' do
-      instance = controller.questionnaire_factory('ReviewQuestionnaire')
-      expect(instance).to be_a(ReviewQuestionnaire)
-    end
-
-    it 'sets flash error and returns nil for an invalid type' do
-      flash = {}
-      allow(controller).to receive(:flash).and_return(flash)
-
-      result = controller.questionnaire_factory('InvalidType')
-      expect(result).to be_nil
-      expect(flash[:error]).to eq('Error: Undefined Questionnaire')
-    end
-
-    it 'handles nil or empty type string' do
-      flash = {}
-      allow(controller).to receive(:flash).and_return(flash)
-
-      result = controller.questionnaire_factory(nil)
-      expect(result).to be_nil
-      expect(flash[:error]).to eq('Error: Undefined Questionnaire')
-    end
+    
+    context 'when some scores are missing advice' do
+      it 'creates only missing advice entries' do
+        (1..3).each do |score|
+          if score == 2
+            # Simulate missing advice
+            allow(QuestionAdvice).to receive(:where)
+              .with('question_id = ? AND score = ?', question.id, score)
+              .and_return([])
+    
+            # Expect creation only for missing score
+            expect(question.question_advices).to receive(:<<)
+              .with(have_attributes(score: score))
+          else
+            advice = double('QuestionAdvice')
+            allow(QuestionAdvice).to receive(:where)
+              .with('question_id = ? AND score = ?', question.id, score)
+              .and_return([advice])
+    
+            # Expect NO creation for scores that already have advice
+            expect(question.question_advices).not_to receive(:<<)
+              .with(have_attributes(score: score))
+          end
+        end
+    
+        allow(QuestionAdvice).to receive(:delete).with(any_args)
+    
+        QuestionnaireHelper.adjust_advice_size(questionnaire, question)
+      end
+    end       
   end
 end
